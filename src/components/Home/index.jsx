1f47f4a0a1d814870dce760bdef70ce3c666052e
TongchengQiu <TongchengQiu@gmail.com>
2016-05-08 01:07:58 +0800

    complete music player

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
 
-export default function Home() {
-  return (
-    <div className="view-home">
-      <h1>HOME</h1>
-      <button onClick={() => { browserHistory.goBack(); }}>back</button>
-    </div>
-  );
+import MusicPlayer from '../MusicPlayer';
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        src: 'http://120.192.249.202:9090/data6/b/0/94/7/667fbf52b4f83b0ee8b6deb03d87940b/music.126.net/62838e55d7c94478db1139a005dcd69c.mp3?type=126music',
+        name: '给自己的信'
+      }
+    };
+  }
+
+  test() {
+    this.setState({
+      data: {
+        src: 'http://111.20.250.39/m10.music.126.net/20160508002202/e3866bc0a574bc5fb0ed0e2d007d35ab/ymusic/c71a/13a9/d984/45e23cb3a5c75802963bf30bf5d5c927.mp3?wshc_tag=0&wsts_tag=572e104e&wsid_tag=de2994fe&wsiphost=ipdbm',
+        name: '123123'
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="view-home">
+        <button onClick={() => { browserHistory.goBack(); }}>back</button>
+        <MusicPlayer data={this.state.data} next={() => this.test()} />
+      </div>
+    );
+  }
 }
---
+++ src/components/MusicPlayer/index.jsx
@@ -0,0 +1,177 @@
+import React from 'react';
+
+require('./index.scss');
+
+export default class MusicPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null,
+      allTime: 0,
+      nowTime: 0,
+      isPlay: false,
+      isLoop: false,
+      volumn: 3,
+      isVolumeShow: false,
+      isMuted: false
+    };
+  }
+
+  componentDidMount() {
+    const audio = this.refs.audio;
+    audio.addEventListener('ended', this.audioEnd.bind(this), false);
+    audio.addEventListener('loadedmetadata', this.audioLoadedMetaData.bind(this), false);
+    audio.addEventListener('timeupdate', this.audioTimeUpdate.bind(this), false);
+    audio.volume = (this.state.volumn / 5).toFixed(1);
+  }
+
+  componentWillUnmount() {
+    const audio = this.refs.audio;
+    audio.removeEventListener('ended', this.audioEnd.bind(this), false);
+    audio.removeEventListener('loadedmetadata', this.audioLoadedMetaData.bind(this), false);
+    audio.removeEventListener('timeupdate', this.audioTimeUpdate.bind(this), false);
+  }
+
+  audioLoadedMetaData() {
+    this.setState({
+      allTime: this.refs.audio.duration
+    });
+  }
+
+  audioEnd() {
+    this.handleNext();
+  }
+
+  audioTimeUpdate() {
+    const audio = this.refs.audio;
+    this.setState({
+      nowTime: audio.currentTime
+    });
+  }
+
+  handlePlay() {
+    this.setState({
+      isPlay: !this.state.isPlay
+    }, () => {
+      if (this.state.isPlay) {
+        this.refs.audio.pause();
+      } else {
+        this.refs.audio.play();
+      }
+    });
+  }
+
+  handlePrev() {
+    this.props.prev();
+  }
+
+  handleNext() {
+    this.props.next();
+  }
+
+  handleMute() {
+    this.setState({
+      isMuted: !this.state.isMuted
+    }, () => {
+      this.refs.audio.muted = this.state.isMuted;
+    });
+  }
+
+  handleType() {
+    this.setState({
+      isLoop: !this.state.isLoop
+    });
+  }
+
+  changeVolume(n) {
+    this.setState({
+      volumn: n,
+      isVolumeShow: false
+    });
+    this.refs.audio.volume = (n / 5).toFixed(1);
+  }
+
+  toggleVolume() {
+    this.setState({
+      isVolumeShow: !this.state.isVolumeShow
+    });
+  }
+
+  transformTime(seconds) {
+    function rDouble(num) {
+      return num.toString().length === 1 ? `0${num}` : `${num}`;
+    }
+    return `${rDouble(Math.floor(Math.floor(seconds) / 60))}:${rDouble(Math.floor(seconds) % 60)}`;
+  }
+
+  render() {
+    const { data } = this.props;
+    const { allTime, nowTime, isPlay, isLoop, volumn, isVolumeShow, isMuted } = this.state;
+    return (
+      <div className="music-player">
+        <audio
+          src={data.src}
+          autoPlay
+          loop={isLoop}
+          style={{ display: 'none' }}
+          ref="audio"
+        >
+        </audio>
+        <div className="options-box">
+          <div className="prev-btn" onClick={(e) => this.handlePrev(e)} />
+          <div
+            className={`play-btn ${isPlay ? 'play' : 'pause'}`}
+            onClick={(e) => this.handlePlay(e)}
+          />
+          <div className="next-btn" onClick={(e) => this.handleNext(e)} />
+        </div>
+        <div className="progress-box">
+          <div className="music-name">{data.name}</div>
+          <div className="progress-bar">
+            <div
+              className="progress-inner"
+              style={{
+                width: `${(nowTime / allTime) * 100}%`
+              }}
+            />
+          </div>
+          <div className="show-times">
+            <span>{this.transformTime(nowTime)}</span>
+            <span>/</span>
+            <span>{this.transformTime(allTime)}</span>
+          </div>
+          <div className="volume-box">
+            <span className="name" onClick={() => this.toggleVolume()}>音量</span>
+            {
+              isVolumeShow &&
+                <ul className="adjust-box">
+                  {
+                    [5, 4, 3, 2, 1].map((i) => (
+                      <li
+                        key={`v-${i}`}
+                        onClick={() => { this.changeVolume(i); }}
+                        className={i <= volumn ? 'active' : null}
+                      />
+                    ))
+                  }
+                </ul>
+            }
+            <span
+              className="mute-btn"
+              onClick={() => this.handleMute()}
+            >
+              {isMuted ? '🔇' : '🔊'}
+            </span>
+          </div>
+        </div>
+        <div
+          className="play-type"
+          onClick={() => this.handleType()}
+          title={isLoop ? '单曲循环' : '顺序播放'}
+        >
+          {isLoop ? '♳' : '♻︎'}
+        </div>
+      </div>
+    );
+  }
+}
